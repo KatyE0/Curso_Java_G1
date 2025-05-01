dcf37c600bbf4a492b4bac3520223858064de34d
KaryEO <escalantekarina.contacto@gmail.com>
2025-04-30 22:38:00 -0600

    Reubicación de archivos al directorio correcto

---
+++ Seccion_01/Reto_02/Entrada_Record.java
@@ -0,0 +1,6 @@
+
+public record Entrada_Record(String event, double precio) {
+    public void mostrarInformacion() {
+      System.out.println("Evento: " + event + " | Precio: $" + precio);
+    }
+   }
