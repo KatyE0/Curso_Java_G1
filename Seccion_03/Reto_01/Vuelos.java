8ac94844085c35370a18eb524fa47e147db511a0
KaryEO <escalantekarina.contacto@gmail.com>
2025-04-30 22:14:56 -0600

    Adición de los Retos 1 y 2 de la seccion 3

---
+++ Seccion_03/Reto_01/Vuelos.java
@@ -0,0 +1,51 @@
+public class Vuelos {
+    // Atributos
+    public static final String codigoVuelo = "Kary123-Vuelos";  // Esto es static y final para que sea único y constante
+    private String destino;
+    private String horaSalida;
+    private String asientoReservado; 
+
+    // Constructor
+    public Vuelos(String destino, String horaSalida) {
+        this.destino = destino;
+        this.horaSalida = horaSalida;
+    }   
+    // Métodos
+    
+    // Método para reservar asiento
+    public boolean reservarAsiento(String nombre, String pasaporte) {
+        if (asientoReservado == null) {
+            asientoReservado = nombre;
+            return true;
+        } else {
+            return false;
+        }
+    }
+   
+    // Este es el método sobrecargado para reservar asiento
+    public boolean reservarAsiento(Pasajero p) {
+        if (asientoReservado == null) {
+            asientoReservado = p.getNombre() + " " + p.getPasaporte();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // Método para cancelar reserva
+    public void cancelarReserva() {
+        asientoReservado = null;
+        System.out.println("❌ Cancelando reserva...");
+    } 
+    
+    // Método para obtener itinerario
+    public String obtenerItinerario() {
+        return "\n Itinerario del vuelo \n\nVuelo: " + codigoVuelo + "\nDestino: " + destino + "\nSalida: " + horaSalida +
+               "\nPasajero: " + (asientoReservado != null ? asientoReservado : "No hay reserva") + "\n";
+    }
+
+    // Método para obtener el código del vuelo ya que es final y no se puede modificar
+    public String getCodigoVuelo() {
+        return codigoVuelo;
+    }   
+}
