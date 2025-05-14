033ecee123167c3225d1f2bc84caab0e0c2a5d97
KaryEO <karinaesol220305@gmail.com>
2025-05-13 21:32:38 -0600

    Renombre carpeta

---
+++ Sesion_01/Reto_02/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args) {
+            // Primeras dos entradas manuales
+            Entrada entrada_1 = new Entrada("Concierto feliz", 225.50);
+            Entrada entrada_2 = new Entrada("Obra de teatro", 1225.50);
+
+            //Uso del metodo para mostrar la informacion 
+            entrada_1.mostrarInformacion();
+            entrada_2.mostrarInformacion();
+
+            //Reto adicional
+
+            //Entrada nueva
+            Entrada_Record entrada_3 = new Entrada_Record("Festival feliz", 256.23);
+
+            // Muestra el toString() generado automáticamente por el record.
+            System.out.println(entrada_3);
+        
+    }
+}
+
